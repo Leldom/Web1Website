ba88df3106001c3a7103d7716319f69e132bb32e
Leldom <Kannix1402@gmail.com>
2025-02-04 18:12:59 +0100

    figgen

--- TestWebsite/javascript/map.js
+++ TestWebsite/javascript/map.js
@@ -9,3 +9,16 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+var map = L.map('mapIndex').setView([34.9677, 135.7792], 7);
+var marker = L.marker([34.9677, 135.7792]).addTo(mapIndex);
+var marker = L.marker([35.3606, 138.7271]).addTo(mapIndex);
+var marker = L.marker([35.03935743038964, 135.7292476622747]).addTo(mapIndex);
+var marker = L.marker([35.659463908087375, 139.70050439544764]).addTo(mapIndex);
+var marker = L.marker([34.39262490280143, 132.45225395452914]).addTo(mapIndex);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mapIndex);
